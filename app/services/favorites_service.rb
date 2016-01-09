44c0ea753db358f22dd0c7338366be1da6b68c7a
cameronjacoby <cameron.jacoby@gmail.com>
2016-01-08 17:01:37 -0800

    move favoriting into service

---
+++ app/services/favorites_service.rb
@@ -0,0 +1,20 @@
+class FavoritesService
+
+	def favorite(user, post_id)
+		if favorited_post = Post.find_by(id: post_id)
+		  unless user.favorite_posts.include?(favorited_post)
+		    Favorite.create(user_id: user.id, post_id: post_id)
+		  end
+		end
+	end
+
+	def unfavorite(user, post_id)
+		unfavorites = Favorite.where(user_id: user.id, post_id: post_id)
+		if unfavorites.any?
+		  unfavorites.each do |unfavorite|
+		    unfavorite.destroy
+		  end
+		end
+	end
+
+end
